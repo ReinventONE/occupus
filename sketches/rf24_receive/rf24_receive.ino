586fb38d718891f89719d1776cb93878d384feca
Konstantin Gredeskoul <kigster@gmail.com>
2014-08-10 01:29:32 -0700

    Major refactor, and switching to timers everywhere

--- sketches/rf24_receive/rf24_receive.ino
+++ sketches/rf24_receive/rf24_receive.ino
@@ -6,13 +6,13 @@
 /*
  * Connections:
  *
- 	GND		GND
-  	3V3		3V3
-	CE		9
-	CSN		10
-	SCK		13
-	MOSI	11
-	MISO	12
+ GND		GND
+ 3V3		3V3
+ CE		9
+ CSN		10
+ SCK		13
+ MOSI	11
+ MISO	12
 
  */
 
@@ -20,7 +20,9 @@
 #include "nRF24L01.h"
 #include "RF24.h"
 #include "printf.h"
+#include <SimpleTimer.h>
 
+#define INACTIVITY_TIMEOUT 5000
 //
 // Hardware configuration
 //
@@ -28,27 +30,47 @@
 // Set up nRF24L01 radio on SPI bus plus pins 9 & 10
 
 RF24 radio(9, 10);
+SimpleTimer timer(1);
+typedef struct publisherInfoStruct {
+	uint8_t led;
+	bool status;
+	bool connected;
+	uint64_t pipe;
+	uint8_t senderId;
+	unsigned long lastTransmissionAt;
+} publisherInfo;
 
-const uint8_t receiveLed[] = {7,8};
-static const uint8_t transmitters = sizeof(receiveLed) / sizeof(uint8_t);
-static bool status[transmitters];
+publisherInfo publisher[] = {
+		{ 7, false, false, 0xF0F0F0F0E1LL, 0x010, 0 },
+		{ 8, false, false, 0xF0F0F0F0D2LL, 0x020, 0 }
+};
 
-//
-// Topology
-//
+static const uint8_t transmitters = sizeof(publisher) / sizeof(publisherInfo);
 
-const uint64_t pipes[] 		= { 0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL	};
-const uint8_t states[] 		= { 0x0, 			0x1 			}; // false or true
-const uint8_t senders[] 	= { 0x010, 			0x020 			}; // on, off
-
-void blink() {
+void showStatus(int timerId) {
 	for (int i = 0; i < transmitters; i++) {
-		digitalWrite(receiveLed[i], status[i] ? HIGH : LOW);
+		digitalWrite(publisher[i].led, publisher[i].status ? HIGH : LOW);
+		printf("transmitter %d is %7s, status = %3s\n", i+1,
+				publisher[i].connected? "online" : "offline",
+				publisher[i].status ? "ON" : "OFF");
+	}
+	printf("\n");
+}
+
+void resetDeadRadios(int timerId) {
+	for (int i = 0; i < transmitters; i++) {
+		if (millis() - publisher[i].lastTransmissionAt > INACTIVITY_TIMEOUT
+				&& publisher[i].status) {
+			printf("resetting dead radio %d: last heard %d seconds ago\n",
+					i+1,
+					(int)((millis() - publisher[i].lastTransmissionAt) / 1000));
+			publisher[i].status = false;
+			publisher[i].connected = false;
+		}
 	}
 }
 
 void setup(void) {
-
 	Serial.begin(57600);
 
 	printf_begin();
@@ -61,17 +83,18 @@ void setup(void) {
 	// optionally, reduce the payload size.  seems to improve reliability
 	radio.setPayloadSize(8);
 
-	radio.openReadingPipe(1, pipes[0]);
-	radio.openReadingPipe(2, pipes[1]);
+	radio.openReadingPipe(1, publisher[0].pipe);
+	radio.openReadingPipe(2, publisher[1].pipe);
 
 	radio.startListening();
 	radio.printDetails();
 
 	for (int i = 0; i < transmitters; i++) {
-		pinMode(receiveLed[i], OUTPUT);
-		status[i] = false;
+		pinMode(publisher[i].led, OUTPUT);
 	}
 
+	timer.setInterval(2000, &resetDeadRadios);
+	timer.setInterval(990,  &showStatus);
 }
 void loop(void) {
 
@@ -82,15 +105,17 @@ void loop(void) {
 		while (!done) {
 			// Fetch the payload, and see if this was the last one.
 			done = radio.read(&data, sizeof(data));
-			printf("got data [%d]\n", data);
+			// printf("got data [%d]\n", data);
 			for (int i = 0; i < transmitters; i++) {
-				if ((data & senders[i]) == senders[i]) {
-					status[i] = data & 1;
-					printf("status for %d is %d", i, status[i]);
+				if ((data & publisher[i].senderId) == publisher[i].senderId) {
+					publisher[i].connected = true;
+					publisher[i].status = data & 1;
+					publisher[i].lastTransmissionAt = millis();
+					// printf("status for %d is %d", i, publisher[i].status);
 				}
 			}
+			timer.run();
 		}
-		printf("\n");
-		blink();
 	}
+	timer.run();
 }
--- sketches/rf24_transmit/rf24_transmit.ino
+++ sketches/rf24_transmit/rf24_transmit.ino
@@ -18,73 +18,148 @@
 
 #include "Sonar.h"
 #include "MotionSensor.h"
+#include <RotaryEncoderWithButton.h>
+#include <SimpleTimer.h>
 
+#define SENDER_0
+
+#ifdef SENDER_0
+// pinA, pinB, pinButton
+RotaryEncoderWithButton rotary(2,3,4);
+
+//// Sender #0
+uint8_t pinStatusLed = 6,
+		pinMotionLed = 7,
+		pinSonarLed = 5,
+
+		pinIRInput = A1,
+		pinPhotoCell = A0,
+		pinSonarTrigger = A2,
+		pinSonarEcho = A3;
+
+const uint8_t me 		= 0; // 0 or 1 (offset into senders[]) and pipes[]
+#endif
+
+#ifdef SENDER_1
+
+// Sender #1
 uint8_t pinStatusLed = 2,
-		pinPIRLed = 3,
+		pinMotionLed = 3,
 		pinSonarLed = 7,
+
 		pinIRInput = 6,
 		pinPhotoCell = A0,
 		pinSonarTrigger = 5,
 		pinSonarEcho = 4;
 
+const uint8_t me 		= 1; // 0 or 1 (offset into senders[]) and pipes[]
+
+#endif
 
 Sonar sonar(
 		pinSonarTrigger,
 		pinSonarEcho,
-		300,  // Maximum distance we want to ping for (in centimeters).
+		200,  // Maximum distance we want to ping for (in centimeters).
 			  // Maximum sensor distance is rated at 400-500cm.
-		150); // Recognize objects within this many centimeters
+		90); // Recognize objects within this many centimeters
 
 MotionSensor motion(pinIRInput, 5000);
 
 RF24 radio(9, 10);
+SimpleTimer timer(1);
 
 typedef struct OccupancyStruct {
 	bool occupied;
 	unsigned long occupiedAt;
 	unsigned long vacatedAt;
 	unsigned long wasOccupiedAt;
+	bool lightDetected;
+	bool motionDetected;
+	bool sonarDetected;
 } occupancyStatus;
 
 unsigned int occupancyGracePeriod = 5000; // keep status as "occupied" after it goes off to avoid flickering
 occupancyStatus occupancy;
 
-const uint8_t states[] 	= { 0x000, 0x001 }; // VACANT, OCCUPIED, ...
+typedef struct senderInfoStruct {
+	bool connected;
+	uint64_t pipe;
+	uint8_t senderId;
+} senderInfo;
 
-// Radio pipe addresses for the 2 nodes to communicate.
-// 							client1         client2
-const uint64_t pipes[] 	= { 0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL };
-const uint8_t senders[] = { 0x010, 			0x020 }; // first sender, second, etc.
-const uint8_t me 		= 1; // 0 or 1 (offset into senders[]) and pipes[]
-const unsigned int sendPeriod = 500;
-unsigned long lastSent 	= 0;
+senderInfo senders[] = {
+		{ false, 0xF0F0F0F0E1LL, 0x010 },
+		{ false, 0xF0F0F0F0D2LL, 0x020 }
+};
 
-
-void showStatus() {
-	digitalWrite(pinStatusLed, occupancy.occupied ? HIGH : LOW);
-}
-
-void sendStatus() {
-	if (millis() - lastSent > sendPeriod) {
-		lastSent = millis();
-
-		unsigned long data = occupancy.occupied | senders[me];
-//		printf("transmitting [%s], me=[%d], data=[%d]\n",
-//				occupancy.occupied ? "OCCIPIED" : "VACANT", me, (int) data);
-
-		bool ok = radio.write(&data, sizeof(data));
-		if (!ok)
-			printf("error sending data over RF24\n");
-	}
-}
-
-bool isRoomDark() {
-	int photoCellReading = analogRead(pinPhotoCell);
-	return (photoCellReading < 250);
-}
+senderInfo mySender = senders[me];
 
 void resetOccupancy() {
 	memset(&occupancy, 0x0, sizeof(occupancy));
+}
+
+//____________________________________________________________________________
+//
+// Timers
+
+void showStatus(int timerId) {
+//	digitalWrite(pinStatusLed, occupancy.occupied ? HIGH : LOW);
+	digitalWrite(pinMotionLed, occupancy.motionDetected ? HIGH : LOW);
+	digitalWrite(pinSonarLed, occupancy.sonarDetected ? HIGH : LOW);
+}
+
+void sendStatus(int timerId) {
+	unsigned long data = occupancy.occupied | mySender.senderId;
+	bool ok = radio.write(&data, sizeof(data));
+	if (!ok)
+		printf("error sending data over RF24\n");
+}
+
+void detectLight(int timerId) {
+	int photoCellReading = analogRead(pinPhotoCell);
+	occupancy.lightDetected = (photoCellReading > 250);
+}
+
+
+void detectMotion(int timerId) {
+	occupancy.motionDetected = motion.detectedNonRetriggering();
+}
+
+void detectSonar(int timerId) {
+	occupancy.sonarDetected = sonar.detected();
+}
+
+void analyzeOccupancy(int timerId) {
+	printf("INFO: status: Lights On?: %s, Motion?: %s, Sonar?: %s\n",
+			(occupancy.lightDetected ? "YES" : "NO"),
+			(occupancy.motionDetected ? "YES" : "NO"),
+			(occupancy.sonarDetected ? "YES" : "NO"));
+	bool isSomeoneThere = occupancy.lightDetected
+			&& (occupancy.motionDetected || occupancy.sonarDetected);
+	unsigned long now = millis();
+	if (isSomeoneThere) {
+		if (!occupancy.occupied) {
+			printf(
+					"INFO: detected new occupancy at %d, Lights On?: %s, Motion?: %s, Sonar?: %s\n",
+					(int) (now / 1000),
+					(occupancy.lightDetected ? "YES" : "NO"),
+					(occupancy.motionDetected ? "YES" : "NO"),
+					(occupancy.sonarDetected ? "YES" : "NO"));
+			occupancy.occupiedAt = now;
+			occupancy.vacatedAt = 0;
+		}
+		occupancy.occupied = true;
+		occupancy.wasOccupiedAt = now;
+	} else {
+		if (occupancy.occupied
+				&& ((now - occupancy.wasOccupiedAt) > occupancyGracePeriod)) {
+			printf("INFO: end of occupancy at %d, duration: %d seconds\n",
+					(int) (now / 1000),
+					(int) ((now - occupancy.occupiedAt) / 1000));
+			resetOccupancy();
+			occupancy.vacatedAt = now - occupancyGracePeriod;
+		}
+	}
 }
 
 //–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
@@ -98,58 +173,28 @@ void setup(void) {
 	radio.begin();
 	radio.setRetries(15, 15);
 	radio.setPayloadSize(8);
-	radio.openWritingPipe(pipes[me]);
+	radio.openWritingPipe(mySender.pipe);
 	radio.printDetails();
 
 	pinMode(pinStatusLed, OUTPUT);
-	pinMode(pinPIRLed, OUTPUT);
+	pinMode(pinMotionLed, OUTPUT);
 	pinMode(pinSonarLed, OUTPUT);
 
 	motion.init();
 
 	resetOccupancy();
+
+	timer.setInterval(990,  &showStatus);
+	timer.setInterval(2000,  &sendStatus);
+
+	timer.setInterval(110, 	&detectLight);
+	timer.setInterval(220,  &detectMotion);
+	timer.setInterval(330,  &detectSonar);
+
+	timer.setInterval(1000, &analyzeOccupancy);
 }
 
+
 void loop(void) {
-	bool roomDark = isRoomDark();
-	bool motionDetected = motion.detectedNonRetriggering();
-	bool sonarDetected = sonar.detected();
-
-	printf("INFO: status: Lights On?: %s, Motion?: %s, Sonar?: %s\n",
-			(roomDark ? "NO" : "YES"),
-			(motionDetected ? "YES" : "NO"),
-			(sonarDetected ? "YES" : "NO")
-			);
-
-	bool isSomeoneThere = !roomDark && (motionDetected || sonarDetected);
-	unsigned long now = millis();
-	if (isSomeoneThere) {
-		if (!occupancy.occupied) {
-			printf("INFO: detected new occupancy at %d, Lights On?: %s, Motion?: %s, Sonar?: %s\n",
-					(int) (now / 1000),
-					(roomDark ? "NO" : "YES"),
-					(motionDetected ? "YES" : "NO"),
-					(sonarDetected ? "YES" : "NO")
-					);
-			occupancy.occupiedAt = now;
-			occupancy.vacatedAt = 0;
-		}
-		occupancy.occupied = true;
-		occupancy.wasOccupiedAt = now;
-	} else {
-		if (occupancy.occupied && ((now - occupancy.wasOccupiedAt) > occupancyGracePeriod)) {
-			printf("INFO: end of occupancy at %d, duration: %d seconds\n",
-					(int) (now / 1000),
-					(int) ((now - occupancy.occupiedAt) / 1000)
-					);
-			resetOccupancy();
-			occupancy.vacatedAt = now - occupancyGracePeriod;
-		}
-
-	}
-
-	showStatus();
-	sendStatus();
-
-	delay(500);
+	timer.run();
 }

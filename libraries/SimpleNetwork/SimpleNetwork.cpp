eacb8e2276f9402a7ca12268c775abdca6fede04
Konstantin Gredeskoul <kigster@gmail.com>
2014-08-10 23:34:04 -0700

    Adding stub for simple network

---
+++ libraries/SimpleNetwork/SimpleNetwork.cpp
@@ -0,0 +1,14 @@
+/*
+ * SimpleNetwork.cpp
+ *
+ *  Created on: Aug 10, 2014
+ *      Author: Konstantin Gredeskoul
+ *
+ *  (c) 2014 All rights reserved.  Please see LICENSE.
+ */
+
+#include "SimpleNetwork.h"
+
+SimpleNetwork::SimpleNetwork() {
+}
+
---
+++ libraries/SimpleNetwork/SimpleNetwork.h
@@ -0,0 +1,20 @@
+/*
+ * SimpleNetwork.h
+ *
+ * RF24-based network of up to 5 senders, and 1 receiver.
+ *
+ *  Created on: Aug 10, 2014
+ *      Author: Konstantin Gredeskoul
+ *
+ *  (c) 2014 All rights reserved.  Please see LICENSE.
+ */
+
+#ifndef SIMPLENETWORK_H_
+#define SIMPLENETWORK_H_
+
+class SimpleNetwork {
+public:
+	SimpleNetwork();
+};
+
+#endif /* SIMPLENETWORK_H_ */
